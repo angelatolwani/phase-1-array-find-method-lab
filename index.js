e033268ecf9f6c174ceb3a53146176cff6200887
Angela Tolwani <angela@tolwani.com>
2024-04-30 14:06:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(array) {
+    const winningRecord = array.find(checkResult);
+    if (winningRecord != null) {
+        return winningRecord.year;
+    } else {
+        return undefined;
+    }
+};
+
+
+function checkResult(record, index, array) {
+    return (record.result === "W");
+};
